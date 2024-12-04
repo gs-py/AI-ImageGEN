3959fec4f725ead63703e3f003a19d6c961b2feb
Gladwin <75156799+xakep-8@users.noreply.github.com>
2024-12-04 19:17:44 +0530

    Changed UI elemenst

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
-
+import { RiAiGenerate2 } from "react-icons/ri";
 const Hero = () => {
+  const [isDisabled, setIsDisabled] = useState(false);
     const [image, setImage] = useState(null); // State to store the image
   const [inputText, setInputText] = useState(""); // State to store user input
     const [animate ,setanimate] = useState(false)
@@ -24,6 +25,10 @@ const Hero = () => {
 
     const generateImage = async () => {
       setImage(null)
+      setIsDisabled(true);
+
+    // Enable the button after 5 seconds
+    
     if (!inputText.trim()) {
       alert("Please enter a prompt for the model.");
       return;
@@ -32,12 +37,16 @@ const Hero = () => {
     const blob = await query({inputs: inputText});
       const imageUrl = URL.createObjectURL(blob);
       setanimate(false)
-    setImage(imageUrl); // Update the state with the image URL
+      setImage(imageUrl);
+      setTimeout(() => {
+      setIsDisabled(false);
+    }, 8000);
+      // Update the state with the image URL
   };
 
 
     return (
-        <div className=' w-full h-full  flex justify-center items-center  flex-col sm:flex-row'> 
+        <div className=' w-full h-full  flex justify-center items-center  flex-col sm:flex-row p-4'> 
             <div className='  w-[50%] h-full flex justify-center items-center flex-col gap-2'>
                 <h1 className=' text-2xl text-pretty  font-medium'>Free Online AI Image Generator</h1>
                 <h4 className=' w-[80%] text-center  text-yellow-400 text-sm text-pretty hidden sm:block'>Dream it up, then add it to your design. Watch your words and phrases transform into beautiful images with the best AI image generators available at your fingertips. Stand out with an image perfect for your project.</h4>
@@ -46,13 +55,19 @@ const Hero = () => {
 
                     <input className=' rounded-lg outline text-black p-1 border' type="text"   value={inputText}
                         onChange={(e) => setInputText(e.target.value)} 
-                        placeholder='Enter a prompt' />
-                    
-                    <button className='rounded-lg    p-2 bg-[#778da9]' onClick={generateImage}>
-                        Generate
+              placeholder='Enter a prompt' />
+            
+            <button className={`rounded-lg   text-nowrap flex flex-nowrap items-center gap-2 
+                  disabled={isDisabled} border-t-2   p-2   ${isDisabled ? " cursor-not-allowed  bg-slate-600 " : " bg-blue-500"}`}
+              onClick={generateImage}>
+                       <RiAiGenerate2></RiAiGenerate2> Generate
                     </button>
+                    
 
-                </div>
+          </div>
+          <p className="text-gray-500 text-[8px] italic  text-pretty text-center ">
+          Note: The average image generation time is around 10 seconds.
+        </p>
             </div>
             
             <div className=' bg-white/10  sm:w-[50%] w-full  rounded-lg m-2 h-full flex items-center justify-center'>
